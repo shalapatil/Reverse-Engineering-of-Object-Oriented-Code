85ece8202dbbcc668737784fc3dabf3a095cacd6
mj-jadhav <rajemayur.413@gmail.com>
2013-04-02 23:57:35 +0530

    contains basic class structure.

---
+++ viewClassDiagram/classPattern.java
@@ -0,0 +1,23 @@
+package viewClassDiagram;
+
+import java.awt.*;
+
+public class classPattern implements classMeasurments{
+	
+	public Label className;
+	public List variable;
+	public List funtion;
+	
+
+	
+	void draw(int x,int y)
+	{
+		className.setBounds(x,y,200,20);
+		variable.setBounds(10,30,200,100);
+		funtion.setBounds(10,131,200,100);
+		
+	}
+	
+	
+
+}
